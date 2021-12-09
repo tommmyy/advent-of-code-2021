4afcb2c58ab2ab0093a8b6d556386b644dc673e1
Tomáš Konrády <tomaskonrady@gmail.com>
2021-12-09 06:42:01 +0100

    feat: day9 - 1

--- src/day9/day9.js
+++ src/day9/day9.js
@@ -1,15 +1,40 @@
-import {
-  compose,
-  difference,
-  filter,
-  find,
-  findIndex,
-  forEach,
-  includes,
-  length,
-  map,
-  reduce,
-  sum,
-} from 'ramda';
+import { compose, filter, min, reduce, sum } from 'ramda';
 
-export const vulcanoTubes = data => {};
+const getPoint = (x, y, data) => {
+  try {
+    return data[y][x];
+  } catch (_error) {}
+};
+const getPointByNeigborhood = (x, y, fn, data) =>
+  fn([
+    getPoint(x, y - 1, data),
+    getPoint(x + 1, y, data),
+    getPoint(x, y + 1, data),
+    getPoint(x - 1, y, data),
+  ]);
+
+const getLowestNeigbour = (x, y, data) =>
+  getPointByNeigborhood(
+    x,
+    y,
+    neigborhood =>
+      compose(
+        reduce(min, Infinity),
+        filter(x => x != null),
+      )(neigborhood),
+    data,
+  );
+export const vulcanoTubes = data => {
+  const lows = [];
+  for (let y = 0; y < data.length; y++) {
+    for (let x = 0; x < data[y].length; x++) {
+      const value = getPoint(x, y, data);
+
+      if (getLowestNeigbour(x, y, data) > value) {
+        lows.push(value);
+      }
+    }
+  }
+
+  return sum(lows) + lows.length;
+};
--- src/day9/day9.test.js
+++ src/day9/day9.test.js
@@ -1,34 +1,25 @@
 import path from 'path';
 
-import { compose, map, split, trim } from 'ramda';
+import { compose, map, o, split, trim } from 'ramda';
 import fs from 'fs-extra';
 
 import { stringToList } from '../utils';
 
-import { vulcanoTubes } from './day9';
+import { vulcanoTubes, vulcanoTubes2 } from './day9';
 
-const realData = fs.readFileSync(path.join(__dirname, './input.txt'), {
-  encoding: 'utf-8',
-});
-const w = compose(xs => [...xs].sort(), split(''), trim);
-const parseRow = compose(map(compose(map(w), split(/\s+/))), split(' | '));
+const parseData = compose(map(o(map(Number), split(''))), stringToList);
 
-const parse = map(parseRow);
+const exerciseData = parseData(`2199943210
+3987894921
+9856789892
+8767896789
+9899965678`);
 
-const sanitizeData = compose(parse, stringToList);
-
-const exerciseData =
-  sanitizeData(`be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
-fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
-fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
-aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
-fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
-dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
-bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
-egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
-`);
+const realData = parseData(
+  fs.readFileSync(path.join(__dirname, './input.txt'), {
+    encoding: 'utf-8',
+  }),
+);
 
 describe('day9', () => {
   describe('vulcanoTubes', () => {
@@ -39,8 +30,21 @@ describe('day9', () => {
       });
     };
 
-    testUtil(exerciseData, 26);
+    testUtil(exerciseData, 15);
 
-    testUtil(sanitizeData(realData), 284);
+    testUtil(realData, 539);
+  });
+
+  describe('vulcanoTubes2', () => {
+    const testUtil = (input, expected, only) => {
+      const fn = only ? it.only : it;
+      fn(`should return ${expected}`, () => {
+        expect(vulcanoTubes2(input)).toBe(expected);
+      });
+    };
+
+    testUtil(exerciseData, 15);
+
+    testUtil(realData, 539);
   });
 });
